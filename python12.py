20c49b4bfc26cae8e8ee59409e0e48f769b69fd5
Adarsh2412 <48621642+Adarsh2412@users.noreply.github.com>
2020-04-18 22:40:05 +0530

    Update python12.py

--- python12.py
+++ python12.py
@@ -1,4 +1,11 @@
 class Sweetmenu:
+    menu={}
+    def test(self,name,price):
+        self.name=name
+        self.price=price
+        self.menu[self.name]=self.price
+    def display(self):
+        print(self.menu)
     pass
 
 sweet_menu=Sweetmenu()
@@ -7,5 +14,7 @@ n=int(input('Enter the no. of items you want to add in your menu'))
 for i in range(0,n):
     sweet_menu.name=str(input('Enter the name of the sweet'))
     sweet_menu.price=int(input('Enter the price you want to desginate to the sweet'))
+    sweet_menu.test(sweet_menu.name,sweet_menu.price)
     print(str(sweet_menu.name)+' will cost you around '+str(sweet_menu.price)+' Dollars')
+sweet_menu.display()
       
